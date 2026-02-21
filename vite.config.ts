7fede2f88ecb9c0676ed3f72345088d633c7a0a0
Tahir Tamba <tahir.tamba@gmail.com>
2026-02-20 22:12:25 -0500

    Import QC LiDAR Explorer app into v3

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/qc-lidar-explorer-v3/",
   plugins: [react()],
-  base: "/qc-lidar-explorer/",
 });
